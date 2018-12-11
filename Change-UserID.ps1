90e4a10edfe6b3d955a900c997169d7136211631
Nickjrp <nickjrp@hotmail.com>
2018-12-11 17:38:20 +0000

    Found out we need to change the domains in AD of some of the users as well so have created a script for changing one but, so far, unable to create a script which changes domains from a CSV file.

--- Change-UserID.ps1
+++ Change-UserID.ps1
@@ -1,19 +1,18 @@
 Get-ADUser -filter * | Select SAMAccountName
 
-
-FINDS ALL USERS WITH A SPACE in THE SAMACCOUNTNAME
+#FINDS ALL USERS WITH A SPACE in THE SAMACCOUNTNAME
 get-aduser -filter {samaccountname -like "* *"} | ft
 
-FINDS ALL ACTIVE USERS WITH A SPACE in THE SAMACCOUNTNAME
+#FINDS ALL ACTIVE USERS WITH A SPACE in THE SAMACCOUNTNAME
 get-aduser -filter {(samaccountname -like "* *") -and (enabled -eq $TRUE)} | ft
 
-FINDS ALL ACTIVE USERS WITH A SPACE in THE SAMACCOUNTNAME and NO SURNAME
+#FINDS ALL ACTIVE USERS WITH A SPACE in THE SAMACCOUNTNAME and NO SURNAME
 get-aduser -filter {(samaccountname -like "* *") -and (enabled -eq $TRUE) -and (surname -like "*")} | select -first 10 | Format-Table
 
-TO GET ALL AD PROPERTIES
+#TO GET ALL AD PROPERTIES
 get-aduser -identity <SAMAccountname> -properties *
 
-CODE FOUND ONLINE
+#CODE FOUND ONLINE
 $ADUserList = Import-CSV C:\scripts\ADUserList.csv
 
 foreach ($User in $ADUserList)
@@ -24,7 +23,7 @@ foreach ($User in $ADUserList)
     UserPrincipalName $csvuserprincipalname
 }
 
-EDITED CODE FOUND ONLINE
+#EDITED CODE FOUND ONLINE
 $ADUserList = Import-CSV C:\scripts\ADUserList.csv
 
 foreach ($User in $ADUserList)
@@ -34,5 +33,30 @@ foreach ($User in $ADUserList)
     Get-ADUser $($user.Userid) | Set-Aduser -SamAccountName $csvusername
 }
 
-TO EXPORT LIST BUT DOES NOT INCLUDE EMAIL ADDRESS
-get-aduser -filter {(samaccountname -like "* *") -and (enabled -eq $TRUE) -and (surname -like "*")} | select Givenname,surname,samaccountname,emailaddress | Export-Csv C:\Users\admnjp\Desktop\UsersWithSpace.csv
+#TO EXPORT LIST BUT DOES NOT INCLUDE EMAIL ADDRESS
+get-aduser -filter {(samaccountname -like "* *") -and (enabled -eq $TRUE) -and (surname -like "*")} | select Givenname,surname,samaccountname,emailaddress | Export-Csv C:\REQUIREDPATH.csv
+
+
+
+****************************************************************************************************************************************************************
+
+
+
+#TO SET THE EMAIL ADDRESS TO NEW DOMAINget
+set-aduser -Identity "USERNAME" -userprincipalname "USERNAME@NEWDOMAIN"
+
+#TO FIND A LIST OF USERS WITH REQUIRED DOMAIN
+get-aduser -filter {userprincipalname -like "*@DOMAINTOCHANGE*"} | Export-Csv C:\REQUIREDPATH.csv
+
+#EDITED CODE FROM ABOVE for BULK CHANGING DOMAINS
+
+$ADUserList = Import-CSV C:\PATHTOFILE.csv
+$Domainname = "@domain"
+
+foreach ($User in $ADUserList)
+{
+    
+    $emailaddress = $user.samaccountname$domainname
+    
+    Set-ADUser $User.samaccountname -UserPrincipalName $emailaddress
+}
